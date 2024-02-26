813d90256716a88b8e9e792c83335702a1b421f1
Hamid <hemidbeyli2020@gmail.com>
2024-02-26 07:36:04 -0800

    Added file_storage

--- models/base_model.py
+++ models/base_model.py
@@ -3,29 +3,46 @@
 
 
 import uuid
-import datetime
+from datetime import datetime
+import models
 
 
 class BaseModel:
     """Base class"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Base class constructor"""
-        self.id = uuid.uuid4()
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
-    
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'updated_at' or key == 'created_at':
+                    kwargs[key] = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != '__class__':
+                    self.__dict__[key] = value
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+
     def __str__(self):
         """Str method"""
         return f'[{type(self).__name__}] {self.id} {self.__dict__}'
 
     def save(self):
         """Save method"""
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """Dict method"""
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = type(self).__name__
-        obj_dict['created_at'] = obj_dict.get('created_at').strftime('%Y-%m-%dT%H:%M:%S.%f')
-        obj_dict['updated_at'] = obj_dict.get('updated_at').strftime('%Y-%m-%dT%H:%M:%S.%f')
+        # Ensure created_at and updated_at are datetime objects
+        if isinstance(obj_dict['created_at'], str):
+            obj_dict['created_at'] = datetime.strptime(obj_dict['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+        if isinstance(obj_dict['updated_at'], str):
+            obj_dict['updated_at'] = datetime.strptime(obj_dict['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+        # Now format them as strings
+        obj_dict['created_at'] = obj_dict['created_at'].strftime('%Y-%m-%dT%H:%M:%S.%f')
+        obj_dict['updated_at'] = obj_dict['updated_at'].strftime('%Y-%m-%dT%H:%M:%S.%f')
         return obj_dict
+
