d7477a57a4620fbc49f3d95f5ed7646e8643b69e
Hamid <hemidbeyli2020@gmail.com>
2024-02-26 07:49:23 -0800

    Hot fix

--- models/base_model.py
+++ models/base_model.py
@@ -13,10 +13,18 @@ class BaseModel:
         """Base class constructor"""
         if kwargs:
             for key, value in kwargs.items():
-                if key == 'updated_at' or key == 'created_at':
-                    kwargs[key] = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                if key != '__class__':
-                    self.__dict__[key] = value
+                if key == "id":
+                    self.id = kwargs.get(key)
+                if key == "created_at":
+                    self.created_at = datetime.strptime(kwargs.get(key),
+                                                        '%Y-%m-%dT%H:%M:%S.%f')
+                if key == "updated_at":
+                    self.updated_at = datetime.strptime(kwargs.get(key),
+                                                        '%Y-%m-%dT%H:%M:%S.%f')
+                if key == "my_number":
+                    self.my_number = kwargs.get(key)
+                if key == "name":
+                    self.name = kwargs.get(key)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -28,21 +36,14 @@ class BaseModel:
         return f'[{type(self).__name__}] {self.id} {self.__dict__}'
 
     def save(self):
-        """Save method"""
+        """ Update the update_at """
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """Dict method"""
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = type(self).__name__
-        # Ensure created_at and updated_at are datetime objects
-        if isinstance(obj_dict['created_at'], str):
-            obj_dict['created_at'] = datetime.strptime(obj_dict['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
-        if isinstance(obj_dict['updated_at'], str):
-            obj_dict['updated_at'] = datetime.strptime(obj_dict['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
-        # Now format them as strings
-        obj_dict['created_at'] = obj_dict['created_at'].strftime('%Y-%m-%dT%H:%M:%S.%f')
-        obj_dict['updated_at'] = obj_dict['updated_at'].strftime('%Y-%m-%dT%H:%M:%S.%f')
-        return obj_dict
-
+        """ Returns a dictionary """
+        my_dict = self.__dict__.copy()
+        my_dict['created_at'] = self.created_at.isoformat()
+        my_dict['updated_at'] = self.updated_at.isoformat()
+        my_dict['__class__'] = self.__class__.__name__
+        return(my_dict)
