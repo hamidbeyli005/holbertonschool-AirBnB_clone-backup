43ade8fc485487d0b184f43a3d0826ec0d3b3850
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-02-28 21:54:05 +0400

    Updated storage test case

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,6 +3,7 @@
 
 import unittest
 import os
+import models
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
@@ -32,8 +33,9 @@ class TestFileStorage(unittest.TestCase):
         self.assertTrue(os.path.exists(FileStorage._FileStorage__file_path))
  
     def test_reload(self):
-        self.storage.new(self.model)
-        self.storage.save()
-        self.storage.reload()
+        base_model = BaseModel()
+        models.storage.new(base_model)
+        models.storage.save()
+        models.storage.reload()
         objs = FileStorage._FileStorage__objects
-        self.assertIn("BaseModel." + self.model.id, objs)
+        self.assertIn("BaseModel." + base_model.id, objs)
