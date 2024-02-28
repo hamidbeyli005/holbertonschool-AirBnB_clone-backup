75171b61f94b18f2aceaf05461b687b246a5cc2f
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-02-28 20:43:04 +0400

    revert

--- console.py
+++ console.py
@@ -95,71 +95,30 @@ class HBNBCommand(cmd.Cmd):
                 if key.split(".")[0] == command[0]:
                     print(str(value))
 
-    def do_update(self, argument):
-        """Updates an instance based on the class name and id """
-        tokensU = shlex.split(argument)
-        if len(tokensU) == 0:
+    def do_update(self, arg):
+        """Update instance"""
+        command = arg.split()
+
+        if len(command) == 0:
             print("** class name missing **")
-            return
-        elif len(tokensU) == 1:
+        elif command[0] not in self.__classes:
+            print("** class doesn't exist **")
+        elif len(command) == 1:
             print("** instance id missing **")
-            return
-        elif len(tokensU) == 2:
+        elif len(command) == 2:
             print("** attribute name missing **")
-            return
-        elif len(tokensU) == 3:
+        elif len(command) == 3:
             print("** value missing **")
-            return
-        elif tokensU[0] not in self.classes:
-            print("** class doesn't exist **")
-            return
-        keyI = tokensU[0] + "." + tokensU[1]
-        dicI = models.storage.all()
-        try:
-            instanceU = dicI[keyI]
-        except KeyError:
-            print("** no instance found **")
-            return
-        try:
-            typeA = type(getattr(instanceU, tokensU[2]))
-            tokensU[3] = typeA(tokensU[3])
-        except AttributeError:
-            pass
-        setattr(instanceU, tokensU[2], tokensU[3])
-        models.storage.save()
-
-    def do_count(self, argument):
-        """  retrieve the number of instances of a class """
-        tokensA = shlex.split(argument)
-        dic = models.storage.all()
-        num_instances = 0
-        if tokensA[0] not in self.classes:
-            print("** class doesn't exist **")
-            return
         else:
-            for key in dic:
-                className = key.split('.')
-                if className[0] == tokensA[0]:
-                    num_instances += 1
+            objects = storage.all()
+            key = f"{command[0]}.{command[1]}"
 
-            print(num_instances)
-
-    def precmd(self, argument):
-        """ executed just before the command line line is interpreted """
-        args = argument.split('.', 1)
-        if len(args) == 2:
-            _class = args[0]
-            args = args[1].split('(', 1)
-            command = args[0]
-            if len(args) == 2:
-                args = args[1].split(')', 1)
-                if len(args) == 2:
-                    _id = args[0]
-                    other_arguments = args[1]
-            line = command + " " + _class + " " + _id + " " + other_arguments
-            return line
-        else:
-            return argument
+            if key not in objects:
+                print("** no instance found **")
+            else:
+                obj = objects[key]
+                setattr(obj, command[2], command[3])
+                obj.save()
 
 
 if __name__ == '__main__':
