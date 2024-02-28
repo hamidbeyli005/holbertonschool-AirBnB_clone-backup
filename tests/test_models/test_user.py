dea737114d5ce3bb2099dd98452ca48b0ab03fc8
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-02-28 21:11:40 +0400

    Updated user tests

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -3,24 +3,32 @@
 
 
 import unittest
+import os
+
 from models.user import User
+from models import storage
+
 
 class TestUserModel(unittest.TestCase):
     def setUp(self):
+        if os.path.exists("file.json"):
+            os.remove("file.json")
+        storage.__objects = {}
+
         self.user = User()
         self.user.email = "test@example.com"
         self.user.password = "password123"
         self.user.first_name = "Hamid"
         self.user.last_name = "Hamidbayli"
 
-    def test_email(self):
+    def test_type(self):
+        self.assertIsInstance(self.user.email, str)
+        self.assertIsInstance(self.user.password, str)
+        self.assertIsInstance(self.user.first_name, str)
+        self.assertIsInstance(self.user.last_name, str)
+
+    def test_user_attributes(self):
         self.assertEqual(self.user.email, "test@example.com")
-
-    def test_password(self):
         self.assertEqual(self.user.password, "password123")
-
-    def test_first_name(self):
         self.assertEqual(self.user.first_name, "Hamid")
-
-    def test_last_name(self):
         self.assertEqual(self.user.last_name, "Hamidbayli")
