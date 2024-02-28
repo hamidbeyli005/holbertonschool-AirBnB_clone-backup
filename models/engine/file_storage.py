62e347f24695ce78fb15a5e6732286ffc028c169
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-02-28 16:14:12 +0400

    Updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,31 +20,31 @@ class FileStorage:
 
     def all(self):
         """Returns the dictionary __objects"""
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         key = "{}.{}".format(type(obj).__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        self.__objects[key] = obj
 
     def save(self):
         """serializes __objects to the JSON file"""
-        objects = FileStorage.__objects
+        objects = self.__objects
         my_obj = {}
 
         for key in objects.keys():
             my_obj[key] = objects[key].to_dict()
 
-        with open(FileStorage.__file_path, 'w') as my_file:
+        with open(self.__file_path, 'w') as my_file:
             json.dump(my_obj, my_file)
 
     def reload(self):
         """deserializes the JSON file to __objects"""
-        if os.path.exists(FileStorage.__file_path):
-            with open(FileStorage.__file_path, 'r') as my_file:
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, 'r') as my_file:
                 obj_dicts = json.load(my_file)
                 for key, values in obj_dicts.items():
                     class_name, obj_id = key.split('.')
                     cls = eval(class_name)
                     instance = cls(**values)
-                    FileStorage.__objects[key] = instance
+                    self.__objects[key] = instance
